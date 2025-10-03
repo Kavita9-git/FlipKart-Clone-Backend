710a483e2db079441b0b36cc4a9291c8b9315764
Kavita <kavitaninave123@gmail.com>
2025-10-03 18:56:19 +0530

    update in config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 8081;
 export const BASE_URL = process.env.BASE_URL || "https://flipkart-clone-backend-8b5e.onrender.com";
 export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD || "";
 export const RAZOR_PAY_KEY_ID = process.env.RAZOR_PAY_KEY_ID || "";
